aa7f8fc2824ea26b9b033019022d685e4abaeaa9
risqiyyyy <raisafebriani05@gmail.com>
2023-03-17 00:01:01 +0800

    update

--- application/views/transaksi/diambil.php
+++ application/views/transaksi/diambil.php
@@ -57,8 +57,6 @@
                                                     <td><?php echo $u->dibayar ?></td>
                                                     <td><?php echo $u->nama_user ?></td>
                                                     <td>
-                                                        <a class="btn btn-primary"
-                                                            href="<?php echo base_url(); ?>GeneratePdfController/">Cetak</a>
                                                         <a class="btn btn-success"
                                                             href="<?php echo base_url(); ?>transaksi/detail_transaksi/<?php echo $u->transaksi_id; ?>">Detail</a>
                                                     </td>
